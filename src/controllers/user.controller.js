cb377614b5da8677b53ce5d802301e2a1648d8dd
social-media-backend-clone <vasudobariya1374@gmail.com>
2025-06-19 00:46:12 +0530

    all setup configure

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import asyncHandler from '../utils/asyncHandler.js'
 
 const RegisterUser = asyncHandler( (req ,res) => {
     res.status(200).json({
-        messeage : "Welcome!"
+        messeage : "Vasu Dobariya!"
     })
 })
 
